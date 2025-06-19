938fa29961b18faa4619b28f544a8a8c5db0b314
Krishnakant yadav <113935872+krishnakantyadav0728@users.noreply.github.com>
2025-06-19 14:41:33 +0530

    Create DemoApplication.java

---
+++ DemoApplication.java
@@ -0,0 +1,26 @@
+"cd src/main/java/com/example/demo/"
+"mkdir -p src/main/java/com/example/demo"
+"cd src/main/java/com/example/demo"
+
+
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, OpenShift!";
+    }
+}
+
+
